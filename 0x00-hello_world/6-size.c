23cf2005d9193526210fb336c93c8d9dfb598187
Amicable-Zulu <omnhlapho@gmail.com>
2022-10-13 13:30:27 -0700

    Final

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Returns: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(S)\n", sizeof(char));
+	printf("Size of a char: %i byte(S)\n", sizeof(int));
+	printf("Size of a char: %i byte(S)\n", sizeof(long int));
+	printf("Size of a char: %i byte(S)\n". sizeof(long long int));
+	printf("Size of a char: %i byte(S)\n", sizeof(float));
+	return (0);
+}
